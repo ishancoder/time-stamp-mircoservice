141cd6584cc5f9f27305815555de5c6b5cb3e864
ishancoder <isaxena171@gmail.com>
2016-08-22 11:31:34 +0530

    Completed TimeStamp API

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('<h1>Hello World!</h1>');
+app.use(express.static('views'));
+
+app.get('/:time', function(req, res){
+	var time = req.params.time;
+	var x = parseInt(time);
+	var date = unixTime = naturalTime = null;
+	if (x){
+		date = new Date(x * 1000);
+		unixTime = x;
+		naturalTime = date.toDateString();
+	} else {
+		date = new Date(time);
+		if(date.getTime()){
+			unixTime = date.getTime() / 1000;
+			naturalTime = date.toDateString();
+		}
+	}
+	res.json({
+		unix : unixTime,
+		natural : naturalTime
+	});
 });
 
 app.listen(8080, function(){
